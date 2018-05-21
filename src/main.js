0f842c717b5b634e0e238f127bd4b40c667dc648
mitu <mitusisi@163.com>
2018-05-21 10:06:51 +0800

    more more more

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
@@ -18,7 +19,7 @@ Vue.component("HeaderNav",HeaderNav)
 Vue.component("RightMenu",RightMenu)
 Vue.component('AutomatedForm', AutomatedForm)
 
-
+Vue.use(Vuex)
 Vue.use(lssPlugin)
 Vue.use(ElementUI)
 Vue.use(VueResource)
@@ -26,7 +27,6 @@ Vue.use(VueResource)
 Vue.config.productionTip = false
 
 Vue.prototype.$axios = axios
-
 /* eslint-disable no-new */
 global.vm = new Vue({
   el: '#app',
